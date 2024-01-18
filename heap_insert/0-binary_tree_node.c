69e76d0e62c25db8642b35c910869b208652e4b0
samdaphbynet <lj8@hotmail.fr>
2024-01-18 11:54:53 +0000

    fixed betty style

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -4,28 +4,27 @@
 #include "binary_trees.h"
 
 /**
- * function that creates a binary tree node:
-
+ * binary_tree_node -  function that creates a binary tree node:
  * @parent: is a pointer to the parent node of the node to create
  * @value: is the value to put in the new node
  * When created, a node does not have any children
- * function must return a pointer to the new node, or NULL on failure
+ * Return: function must return a pointer to the new node, or NULL on failure
 */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    if (new_node == NULL) {
-        printf("Memory allocation failed\n");
-        return NULL;
-    }
+if (new_node == NULL)
+{
+printf("Memory allocation failed\n");
+return (NULL);
+}
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
 
-    return new_node;
-    
+return (new_node);
 }
