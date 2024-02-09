f666a9cf8472176ab457f271c6e35f95436ff267
samdaphbynet <lj8@hotmail.fr>
2024-02-09 20:15:02 +0000

    update declaration variables

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,42 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
-int is_palindrome(listint_t **head)
-{
-    if (*head == NULL || (*head)->next == NULL)
-    {
-        return 1;
+/**
+ *  is_palindrome - that checks if a linked list is palindrome
+ *  @head: linked list head
+ *  Return: 1 if is palindrome or 0 if not
+ */
+
+int is_palindrome(listint_t **head) {
+    if (*head == NULL || (*head)->next == NULL) {
+        return 1; // An empty list or a list with only one node is considered a palindrome
     }
 
     listint_t *slow = *head;
     listint_t *fast = *head;
+    listint_t *prev;
+    listint_t *current;
+    listint_t *p1;
+    listint_t *p2;
 
-    while (fast->next != NULL && fast->next->next != NULL)
-    {
+    while (fast->next != NULL && fast->next->next != NULL) {
         slow = slow->next;
         fast = fast->next->next;
     }
 
-    listint_t *prev = NULL;
-    listint_t *current = slow->next;
-
-    while (current != NULL)
-    {
+    prev = NULL;
+    current = slow->next;
+    while (current != NULL) {
         listint_t *next = current->next;
         current->next = prev;
         prev = current;
         current = next;
     }
-
     slow->next = prev;
-    listint_t *p1 = *head;
-    listint_t *p2 = slow->next;
 
-    while (p2 != NULL)
-    {
-        if (p1->n != p2->n)
-        {
+    p1 = *head;
+    p2 = slow->next;
+    while (p2 != NULL) {
+        if (p1->n != p2->n) {
             return 0;
         }
         p1 = p1->next;
@@ -45,9 +45,7 @@ int is_palindrome(listint_t **head)
 
     current = slow->next;
     prev = NULL;
-
-    while (current != NULL)
-    {
+    while (current != NULL) {
         listint_t *next = current->next;
         current->next = prev;
         prev = current;
