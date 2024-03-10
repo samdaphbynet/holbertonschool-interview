ee5149d581c2838fd7bf1ad66b5dfc8e6dd210c1
samdaphbynet <lj8@hotmail.fr>
2024-03-10 22:20:48 +0000

    update validate utf-8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -11,23 +11,9 @@ def validUTF8(data):
     :param data: A list of integers representing the data set.
     :return: True if data is a valid UTF-8 encoding, else False.
     """
-    num_bytes = 0
-
-    for num in data:
-        if num_bytes == 0:
-            if (num >> 3) == 0b11110:
-                num_bytes = 3
-            elif (num >> 4) == 0b1110:
-                num_bytes = 2
-            elif (num >> 5) == 0b110:
-                num_bytes = 1
-            elif (num >> 7) == 0b0:
-                num_bytes = 0
-            else:
-                return False
-        else:
-            if (num >> 6) != 0b10:
-                return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+    try:
+        maskeddata = [n & 255 for n in data]
+        bytes(maskeddata).decode("UTF-8")
+        return True
+    except Exception:
+        return False
