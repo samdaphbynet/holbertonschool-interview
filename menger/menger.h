6d6531223218517dd29c4474370c0a3a09d3647d
samdaphbynet <lj8@hotmail.fr>
2024-03-31 17:20:02 +0000

    header files

---
+++ menger/menger.h
@@ -0,0 +1,16 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+void draw_menger(int level, int x, int y, int size);
+void menger(int level);
+
+
+
+
+#endif
