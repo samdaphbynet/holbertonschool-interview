95f534608d7d069de7c832ab2abd059c42ade07b
samdaphbynet <lj8@hotmail.fr>
2024-05-10 22:58:06 +0200

    header file contain all prototype

---
+++ heap_sort/sort.h
@@ -0,0 +1,14 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void heapify(int *array, size_t size);
+void sift_down(int *array, size_t size, size_t root, size_t max);
+int main(void);
+
+#endif // SORT_H
