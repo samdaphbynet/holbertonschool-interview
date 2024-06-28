828afea1861b215c9e2032cf63620caae6b42110
Crystal ByNet <cristalbynet@gmail.com>
2024-06-28 09:36:14 +0200

    added while inside if statement

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -3,14 +3,27 @@
 #include <stdio.h>
 #include "lists.h"
 
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: address of the node where the loop starts, or NULL if there is no loop
+ */
+
 listint_t *find_listint_loop(listint_t *head) {
     listint_t *fast = head, *slow = head;
 
-    while (fast && fast->next) {
+    while (fast != NULL && fast->next != NULL) {
         slow = slow->next;
         fast = fast->next->next;
 
         if (slow == fast) {
+            slow = head;
+            while (slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
             return slow;
         }
     }
