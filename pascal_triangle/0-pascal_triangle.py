7b6c3238230ba86fbcff2314b4ca7d7a99179b1c
cristalbynet <cristalbynet@gmail.com>
2024-08-19 12:45:39 +0200

    function def pascal_triangle(n): that returns a list of lists of integers

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Returns a list of lists representing the first 'n' lines of Pascal's Triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to the 'n' line.
+    """
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            for j in range(1, i):
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            row.append(1)
+        triangle.append(row)
+    return triangle
