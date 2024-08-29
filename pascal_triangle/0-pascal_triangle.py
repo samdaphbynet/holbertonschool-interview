305fe250048f86b6a16f584a0b5127b7afc6f9ee
cristalbynet <cristalbynet@gmail.com>
2024-08-29 13:08:41 +0200

    update comment

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 """
 Defines function that returns a list of lists of integers
-representing the Pascal's triangle of n
 """
 
 
 def pascal_triangle(n):
     """
     Creates a list of lists of integers representing Pascal's triangle
-
-    parameters:
-        n [int]:
-            the number of rows of Pascal's triangle to recreate
-
-    return:
-        [list of lists of ints]:
-            representation of Pascal's triangle
     """
     triangle = []
     for i in range(n):
